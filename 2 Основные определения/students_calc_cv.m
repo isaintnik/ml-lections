0cedfdeadb29fab4a1f7d8825550037975e2155f
ikuralenok <ikuralenok@gmail.com>
2013-09-19 08:28:21 +0000

    mistakes in octave removed

--- 2 Основные определения/students_calc_cv.m
+++ 2 Основные определения/students_calc_cv.m
@@ -1,25 +1,27 @@
-solveStudents(1);
+clear; clc; close all;
+data = csvread('students.txt');
+i = length(transpose(data(1:1, :)));
+X = data(:, 1:i-1);
+y = data(:, i);
 
-function theta = solveStudents (verbose)
+% Add intercept term to X
+X_norm = X;
+mu = zeros(1, size(X, 2));
+sigma = zeros(1, size(X, 2));
+for i = 1:length(mu)
+    cur_column = X_norm(:, i);
+    mu(i) = mean(cur_column);
+    sigma(i) = std(cur_column);
+    X_norm(:, i) -= mu(i);
+    X_norm(:, i) /= sigma(i);
+endfor
+X=X_norm;
+
+solveStudents(X,y,1);
+
+function [theta, error] = solveStudents (X,y,verbose)
 	%% Load Data
-	data = csvread('students.txt');
-	i = length(transpose(data(1:1, :)));
-	X = data(:, 1:i-1);
-	y = data(:, i);
 	m = length(y);
-
-	% Add intercept term to X
-	X_norm = X;
-	mu = zeros(1, size(X, 2));
-	sigma = zeros(1, size(X, 2));
-	for i = 1:length(mu)
-	    cur_column = X_norm(:, i);
-	    mu(i) = mean(cur_column);
-	    sigma(i) = std(cur_column);
-	    X_norm(:, i) -= mu(i);
-	    X_norm(:, i) /= sigma(i);
-	endfor
-	X=X_norm;
 
 	L=[];
 	yl=[];
@@ -29,46 +31,45 @@ function theta = solveStudents (verbose)
 	rnd=rand(length(y),1);
 	for i = 1:length(y);
 		if (rnd(i,1) > 0.5)
-			%printf("L\n");
 			L=[L; X(i,:)];
 			yl=[yl;y(i)];
 		else
-			%printf("T\n");
 			T=[T; X(i,:)];
 			yt=[yt;y(i)];
 		endif
 	endfor
 
 	% Calculate the parameters from the normal equation
-	b=sum(yl)/m;
+	b=sum(yl)/length(yl);
 	theta = pinv(transpose(L)*L)*transpose(L)*(yl.-b);
 
 	%
-	predictl = transpose((transpose(theta) * transpose(L))).+ b;
+	predictl = L * theta.+ b;
 	diffl = (predictl .- yl) .* (predictl .- yl);
 	resultl = [predictl yl];
 
-	predictt = transpose((transpose(theta) * transpose(T))).+ b;
+	predictt = T * theta.+ b;
 	difft = (predictt .- yt) .* (predictt .- yt);
 	resultt = [predictt yt];
 
 	% Display normal equations result
 	if (verbose != 0)
-		fprintf('Theta computed from the normal equations: \n');
-		fprintf('w_0 &= %.2f ')
+		printf('Theta computed from the normal equations: \n');
+		printf('T_L= %.2f & ', sum(diffl));
+		printf('T_T&= %.2f \\\\ \n', sum(difft));
+		printf('\\sqrt{\\frac{T_L(w_0, b_0)}{n}} = %.2f & ',sqrt(sum(diffl)/length(diffl)));
+		printf('\\sqrt{\\frac{T_T(w_0, b_0)}{n}} = %.2f \\\\ \n',sqrt(sum(difft)/length(difft)));
+
+		printf('\\multicolumn{2}{l}{');
+		printf(" w_0 = (%.2f ")
 		for i = 1:length(theta)
 			if (i != 1)
 				printf(", ");
 			endif
 			printf("%.2f", theta(i));
 		endfor
-		printf("\\\\\n");
-		fprintf('b_0 &= %.2f \\\\ \n', b);
+		printf(")~~ b_0 = %.2f} \\\\ \n", b);
 		fprintf('\\\\ \n');
-		fprintf('T_l&= %.2f \\\\ \n', sum(diffl));
-		fprintf('\\sqrt{\\frac{T_l(w_0, b_0)}{n}} &= %.2f \\\\ \n',sqrt(sum(diffl)/length(diffl)));
-		fprintf('T_t&= %.2f \\\\ \n', sum(difft));
-		fprintf('\\sqrt{\\frac{T_t(w_0, b_0)}{n}} &= %.2f \\\\ \n',sqrt(sum(difft)/length(difft)));
 
 		fprintf('Learn predict and real values: \n');
 		resultl
@@ -77,4 +78,5 @@ function theta = solveStudents (verbose)
 		resultt
 	endif
 	theta=[theta;b];
+	error=sqrt(sum(difft)/length(difft));
 endfunction
--- 2 Основные определения/students_sampling_drop.m
+++ 2 Основные определения/students_sampling_drop.m
@@ -1,17 +1,41 @@
 students_calc_cv;
-t1 = solveStudents(0);
-count=1000;
-s=t1;
-s2=t1.*t1;
-for i = 2:count;
-	ti=solveStudents(0);
-	s=s+ti;
-	ti2=ti.*ti;
-	s2=s2 .+ ti2;
-endfor
-m = s ./ count;
-r = sqrt((s2 .- ((s .* s)./count))./count);
+data = csvread('students.txt');
+i = length(transpose(data(1:1, :)));
+X = data(:, 1:i-1);
+y = data(:, i);
 
-for i = 1:length(m);
-	printf ("{w_0}_{%d} & %.2f \\pm %.2f \\\\\n", i, m(i), r(i));
+X_norm = X;
+mu = zeros(1, size(X, 2));
+sigma = zeros(1, size(X, 2));
+for i = 1:length(mu)
+    cur_column = X_norm(:, i);
+    mu(i) = mean(cur_column);
+    sigma(i) = std(cur_column);
+    X_norm(:, i) -= mu(i);
+    X_norm(:, i) /= sigma(i);
 endfor
+X=X_norm;
+
+
+for f=1:columns(X);
+	Xf = [];
+	if (f > 1);
+		Xf = X(:, 1:f-1);
+	endif
+	if (f < columns(X));
+		Xf = [Xf,X(:, f+1:end)];
+	endif
+
+	totalError = 0;
+	count=1000;
+	for i = 1:count;
+		[ti,error] = solveStudents(Xf,y,0);
+		totalError += error;
+	endfor
+
+	printf("Feature %d error: %.2f\n", f, totalError/count);
+endfor
+
+% for i = 1:length(m);
+% 	printf ("{w_0}_{%d} & %.2f \\pm %.2f \\\\\n", i, m(i), r(i));
+% endfor
