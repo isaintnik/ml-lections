a870d2ff2167825c34480ce863f76fc109a9dcf8
ikuralenok@gmail.com <ikuralenok@gmail.com@1ca92a3d-cb24-933f-6861-9dcab180f1a1>
2013-09-19 08:28:21 +0000

    mistakes in octave removed

--- trunk/2 Основные определения/students_sampling.m
+++ trunk/2 Основные определения/students_sampling.m
@@ -1,16 +1,45 @@
 students_calc_cv;
-t1 = solveStudents(0);
+clc;
+data = csvread('students.txt');
+X = data(:, 1:end-1);
+y = data(:, end);
+
+X_norm = X;
+mu = zeros(1, size(X, 2));
+sigma = zeros(1, size(X, 2));
+for i = 1:length(mu)
+    cur_column = X_norm(:, i);
+    mu(i) = mean(cur_column);
+    sigma(i) = std(cur_column);
+    X_norm(:, i) -= mu(i);
+    X_norm(:, i) /= sigma(i);
+endfor
+X=X_norm;
+
 count=1000;
-s=t1;
-s2=t1.*t1;
-for i = 2:count;
-	ti=solveStudents(0);
-	s=s+ti;
-	ti2=ti.*ti;
-	s2=s2 .+ ti2;
+s=zeros(columns(X) + 1, 1);
+s2=zeros(columns(X) + 1, 1);
+sumError = 0;
+sum2Error = 0;
+for i = 1:count;
+	[ti,error] = solveStudents(X,y,0);
+	s += ti;
+	s2 += ti.*ti;
+	sumError += error;
+	sum2Error += error * error;
 endfor
 m = s ./ count;
 r = sqrt((s2 .- ((s .* s)./count))./count);
+
+theta=m(1:end-1);
+b=m(end);
+
+predict = X * theta .+ b;
+diff = (predict .- y) .* (predict .- y);
+result = [predict y]
+sqrt(sum(diff)/length(diff))
+
+printf("Mean error: %.2f \\pm %.2f\n\n", sumError/count, sqrt((sum2Error - sumError * sumError/count)/count));
 
 for i = 1:length(m);
 	printf ("{w_0}_{%d} & %.2f \\pm %.2f \\\\\n", i, m(i), r(i));
